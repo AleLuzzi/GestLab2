cceab77dc2d7991393e21614806fb2a7d4f658b3
Alessandro Luzzi <alessandro.luzzi77@gmail.com>
2022-08-20 10:25:23 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import kivy.app
+
 from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
-import sqlite3
 import datetime
 from kivy.properties import NumericProperty, StringProperty
-# from kivy.core.window import Window
+from kivy import Config
+import configparser
+import mysql.connector
+
+Config.set('graphics', 'multisamples', '0')
+# Config.set('graphics', 'fullscreen', 'auto')
 
 
 class menu(Screen):
@@ -19,18 +24,36 @@ class IngressoMerce(Screen):
     txtinp = NumericProperty(0)
     fornit = StringProperty()
     articolo = StringProperty()
-    # Connessione al database
-    conn = sqlite3.connect('./data.db',
-                           detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+
+    def indietro(self):
+        print(self.ids.txtinp.text, self.ids.fornit.text, self.ids.articolo.text)
+        self.manager.current = 'menu'
+
+    def leggi_file_ini():
+        ini = configparser.ConfigParser()
+        ini.read('config.ini')
+        return ini
+
+    config_ini = leggi_file_ini()
+
+    conn = mysql.connector.connect(host=config_ini['DataBase']['host'],
+                                   database=config_ini['DataBase']['db'],
+                                   user=config_ini['DataBase']['user'],
+                                   password='')
+
     c = conn.cursor()
 
     lista_tagli = []
     lista_fornitori = []
 
-    for lista in c.execute("SELECT taglio FROM tagli"):
+    c.execute("SELECT taglio FROM tagli")
+
+    for lista in c:
         lista_tagli.extend(lista)
 
-    for lista in c.execute("SELECT azienda FROM fornitori WHERE flag1_ing_merce = 1"):
+    c.execute("SELECT azienda FROM fornitori WHERE flag1_ing_merce = 1")
+
+    for lista in c:
         lista_fornitori.extend(lista)
 
     c.execute("SELECT prog_acq FROM progressivi")
@@ -38,16 +61,11 @@ class IngressoMerce(Screen):
 
     data = datetime.date.today()
 
-    def indietro(self):
-        print(self.ids.txtinp.text, self.ids.fornit.text, self.ids.articolo.text)
-        self.manager.current = 'menu'
-
 
 class main(kivy.app.App):
     def build(self):
-        # Window.size = (1024, 768)
         sm = ScreenManager(transition=WipeTransition())
-        sm.add_widget(menu(name='menu',))
+        sm.add_widget(menu(name='menu'))
         sm.add_widget(IngressoMerce(name='IngressoMerce'))
         return sm
 
