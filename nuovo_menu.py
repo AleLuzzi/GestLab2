49615ff084d4e4ab9eb944f1e5b2d150c54a679a
Alessandro <alessandro.luzzi77@gmail.com>
2023-07-14 08:37:49 +0200

    aggiunto multiselect al recycleview

--- nuovo_menu.py
+++ nuovo_menu.py
@@ -17,7 +17,7 @@ class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
                                  RecycleBoxLayout):
     ''' Adds selection and focus behaviour to the view. '''
 
-class SelectableLabel(RecycleDataViewBehavior, Label):
+class Multicampo_menu(RecycleDataViewBehavior, BoxLayout):
     ''' Add selection support to the Label '''
     index = None
     selected = BooleanProperty(False)
@@ -26,12 +26,12 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
     def refresh_view_attrs(self, rv, index, data):
         ''' Catch and handle the view changes '''
         self.index = index
-        return super(SelectableLabel, self).refresh_view_attrs(
+        return super(Multicampo_menu, self).refresh_view_attrs(
             rv, index, data)
 
     def on_touch_down(self, touch):
         ''' Add selection on touch down '''
-        if super(SelectableLabel, self).on_touch_down(touch):
+        if super(Multicampo_menu, self).on_touch_down(touch):
             return True
         if self.collide_point(*touch.pos) and self.selectable:
             return self.parent.select_with_touch(self.index, touch)
@@ -41,10 +41,10 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
         self.selected = is_selected
 
         rv.data[index]['selected'] = self.selected
-
-
-class Multicampo_menu(BoxLayout):
-    pass
+        if is_selected:
+            print("selection changed to {0}".format(rv.data[index]))
+        else:
+            print("selection removed for {0}".format(rv.data[index]))
 
 
 class Nuovo_menu(Screen):
