974d6f1a7d797c1efa8f6d0228e756fff4eeb653
Alessandro <alessandro.luzzi77@gmail.com>
2023-07-29 10:08:58 +0200

    abilitato multiselect chiudi_lotto

--- chiudi_lotto.py
+++ chiudi_lotto.py
@@ -19,7 +19,7 @@ class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
                                  RecycleBoxLayout):
     ''' Adds selection and focus behaviour to the view. '''
 
-class SelectableLabel(RecycleDataViewBehavior, Label):
+class Multicampo(RecycleDataViewBehavior, BoxLayout):
     ''' Add selection support to the Label '''
     index = None
     selected = BooleanProperty(False)
@@ -28,12 +28,12 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
     def refresh_view_attrs(self, rv, index, data):
         ''' Catch and handle the view changes '''
         self.index = index
-        return super(SelectableLabel, self).refresh_view_attrs(
+        return super(Multicampo, self).refresh_view_attrs(
             rv, index, data)
 
     def on_touch_down(self, touch):
         ''' Add selection on touch down '''
-        if super(SelectableLabel, self).on_touch_down(touch):
+        if super(Multicampo, self).on_touch_down(touch):
             return True
         if self.collide_point(*touch.pos) and self.selectable:
             return self.parent.select_with_touch(self.index, touch)
@@ -43,10 +43,11 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
         self.selected = is_selected
 
         rv.data[index]['selected'] = self.selected
+        if is_selected:
+            print("selection changed to {0}".format(rv.data[index]))
+        else:
+            print("selection removed for {0}".format(rv.data[index]))
 
-
-class Multicampo(BoxLayout):
-    pass
 
 class Chiudi_lotto(Screen):
     def __init__(self, **kwargs):
